6ea63a2f5383cb57d4e79a370c5b1afc56bb357c
Aniket Shitole <91371143+AniketNS@users.noreply.github.com>
2023-04-03 10:47:28 +0530

    Changed Program.cs

--- Ration Shop management/Program.cs
+++ Ration Shop management/Program.cs
@@ -16,7 +16,7 @@ namespace Ration_Shop_management
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SellingForm());
+            Application.Run(new splash());
         }
     }
 }
